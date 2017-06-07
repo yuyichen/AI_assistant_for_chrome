40781b5a6c5003ea375763c20fd85397a27e05ad
lili <lili@zhaozhi.me>
2017-06-07 16:16:48 +0800

    修改了登录相关逻辑,添加右键菜单功能

--- chrome_extension/js/app.js
+++ chrome_extension/js/app.js
@@ -1,11 +1,10 @@
-var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
 
 //判断用户登录状态
 chrome.cookies.get({ url: 'https://aiwriting.io/', name: 'user' }, function(cookie) {
     if (cookie) {
         document.getElementById('loginBox').style.display = "none";
         document.getElementById('saveBox').style.display = "block";
-        document.getElementById('userAccount').innerHTML = localStorage['userAccount'];
+        document.getElementById('userAccount').innerText = localStorage['userAccount'];
         // chrome.browserAction.setBadgeText({ text: '' });
     } else {
         document.getElementById('loginBox').style.display = "block";
@@ -49,11 +48,12 @@ document.getElementById('loginBtn').onclick = function() {
                 
                 if (/请输入验证码/.test(response)) {
                     resultBox.style.display = "block";
-                    resultBox.innerHTML = '登录失败';
+                    resultBox.innerText = '登录失败';
                 } else {
-                    resultBox.innerHTML = '';
-                    document.getElementById('userAccount').innerHTML = account;
+                    resultBox.innerText = '';
+                    document.getElementById('userAccount').innerText = account;
                     localStorage['userAccount']=account;
+                    chrome.browserAction.setIcon({path:'../icon.png'});
                     document.getElementById('loginBox').style.display = "none";
                     document.getElementById('saveBox').style.display = "block";
                     resultBox.style.display = "none";
@@ -65,12 +65,12 @@ document.getElementById('loginBtn').onclick = function() {
                 console.error(xhr, status);
                 var resultBox = document.getElementById('saveResult');
                 resultBox.style.display = "block";
-                resultBox.innerHTML = '请求出错';
+                resultBox.innerText = '请求出错';
             }
         })
     } else {
     	resultBox.style.display = "block";
-        resultBox.innerHTML = '请输入用户名和密码';
+        resultBox.innerText = '请输入用户名和密码';
     }
 }
 
@@ -80,6 +80,8 @@ document.getElementById('logoutLink').onclick = function() {
         url: 'https://aiwriting.io/logout',
         type: 'get',
         success: function(response) {
+            chrome.browserAction.setIcon({path:'../icon_gray.png'});
+            localStorage.removeItem('userAccount');
         	document.getElementById('loginBox').style.display = "block";
             document.getElementById('saveBox').style.display = "none";
             document.getElementById('saveResult').style.display = "none";
@@ -88,7 +90,7 @@ document.getElementById('logoutLink').onclick = function() {
             console.error(xhr, status);
             var resultBox = document.getElementById('saveResult');
             resultBox.style.display = "block";
-            resultBox.innerHTML = '请求出错';
+            resultBox.innerText = '请求出错';
         }
     })
 }
@@ -98,27 +100,49 @@ document.getElementById('logoutLink').onclick = function() {
 //保存文章
 var myUrl = 'https://aiwriting.io/';
 chrome.tabs.getSelected(null, function(tab) {
-    myUrl = tab.url;
+    var clipbordContent = getClipBordContent();
+    if(/^http[s]?:\/\//.test(clipbordContent)){
+        myUrl = clipbordContent;//剪贴板内容是网址的时候
+    }else if(/^http[s]?:\/\//.test(tab.url)){
+        myUrl = tab.url;
+    }else{
+        myUrl="";
+    }
+    
     document.getElementById('aiUrlInput').value = myUrl;
-    var encodedUrl = Base64.encode(myUrl);
+    // document.getElementById('aiUrlInput').focus();
+    // document.execCommand('paste');
     document.getElementById('aiSaveBtn').onclick = function() {
-        if (encodedUrl && encodedUrl.trim()) {
-            $.ajax({
-                url: 'https://aiwriting.io/material/json/import/page/url?url=' + encodedUrl,
-                type: 'get',
-                success: function(res) {
-                    var resultBox = document.getElementById('saveResult');
-                    resultBox.style.display = "block";
-                    if (res.status == "success") {
-                        resultBox.innerHTML = '保存成功';
-                        setTimeout(function() {
-                            window.close();
-                        }, 2000);
-                    } else {
-                        resultBox.innerHTML = res.message;
-                    }
+        if (myUrl && myUrl.trim()) {
+            chrome.runtime.sendMessage({saveLink: myUrl}, function(res) {
+                console.log(res);
+                var resultBox = document.getElementById('saveResult');
+                resultBox.style.display = "block";
+                if (res.status == "success") {
+                    resultBox.innerText = '网页内容抓取，抓取成功';
+                    setTimeout(function() {
+                        window.close();
+                    }, 2000);
+                } else {
+                    resultBox.innerText = res.message;
                 }
-            })
+            });
         }
     }
 });
+
+/**
+ * 获取剪贴板内容
+ * @return {[type]} [description]
+ */
+function getClipBordContent(){
+    var inputDom = document.createElement('input');
+    inputDom.style.position = "absolute";
+    inputDom.style.top = "-99999px";
+    document.body.appendChild(inputDom);
+    inputDom.focus();
+    document.execCommand('paste');
+    return inputDom.value;
+}
+
+
--- chrome_extension/js/background.js
+++ chrome_extension/js/background.js
@@ -1,12 +1,100 @@
-// chrome.cookies.onChanged.addListener(function (cookieItem,cause){
-// 	console.log(cookieItem,cause)
-// 	if(cookieItem.removed && cookieItem.cookie.name=='user'){
-// 		chrome.runtime.sendMessage({loginAction: "logout"}, function(response) {
-// 		  console.log(response);
-// 		});
-// 	}else if(cookieItem.cookie.name=='user' && cookieItem.cause == 'explicit'){
-// 		chrome.runtime.sendMessage({loginAction: "login"}, function(response) {
-// 		  console.log(response);
-// 		});
-// 	}
-// })
+//判断用户登录状态
+chrome.cookies.get({ url: 'https://aiwriting.io/', name: 'user' }, function(cookie) {
+    if (!cookie) {
+    	chrome.browserAction.setIcon({path:'../icon_gray.png'});
+    } else{
+    	chrome.browserAction.setIcon({path:'../icon.png'});
+    }
+})
+
+// 
+/**
+ * 保存链接
+ * @param  {[type]}   myUrl [链接地址]
+ * @param  {Function} cb    [回调]
+ * @return {[type]}         [description]
+ */
+function saveUrl(myUrl,cb){
+    var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
+
+    var encodedUrl = Base64.encode(myUrl);
+    $.ajax({
+    	async:false,//必须为同步方式,否则onMessage中的sendResponse无法发送数据
+        url: 'https://aiwriting.io/material/json/import/page/url?url=' + encodedUrl,
+        type: 'get',
+        success: function(res) {
+            cb && cb(res);
+        }
+    })
+}
+
+//收藏本页
+chrome.contextMenus.create({id:'savePageToAiChuang',title:'收藏本页到我的爱创',contexts:['page']}, function (error){
+    console.log(error)
+});
+
+//链接右键
+chrome.contextMenus.create({id:'saveUrlToAiChuang',title:'收藏链接文章到我的爱创',contexts:['link']}, function (error){
+    console.log(error)
+});
+
+//图片右键
+chrome.contextMenus.create({id:'saveImgToAiChuang',title:'收藏图片到我的爱创',contexts:['image']}, function (error){
+    console.log(error)
+});
+
+//选择文本右键
+chrome.contextMenus.create({id:'saveSelectionToAiChuang',title:'收藏文字到我的爱创',contexts:['selection']}, function (error){
+    console.log(error)
+});
+
+
+console.log(localStorage['userAccount'])
+
+// 右键触发的保存事件
+chrome.contextMenus.onClicked.addListener(function(info){
+	console.log(info)
+	if(!localStorage['userAccount']){
+		alert("请先登录爱创")
+		return;
+	}
+	switch(info.menuItemId){
+		case "savePageToAiChuang":
+		saveUrl(info.pageUrl, function(res) {
+		    if (res.status == "success") {
+		        alert('网页内容抓取，抓取成功');
+		    } else {
+		        alert(res.message);
+		    }
+		})
+		break;
+		case "saveUrlToAiChuang":
+		saveUrl(info.linkUrl, function(res) {
+		    if (res.status == "success") {
+		        alert('网页内容抓取，抓取成功');
+		    } else {
+		        alert(res.message);
+		    }
+		})
+		break;
+		case "saveImgToAiChuang":
+		alert(info.srcUrl);
+		break;
+		case "saveSelectionToAiChuang":
+		alert(info.selectionText);
+		break;
+	}
+   
+})
+
+// 弹窗的保存事件
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) { 
+    if (request.saveLink) {
+        saveUrl(request.saveLink, function(res) {
+        	console.log(res)
+	    	sendResponse(res);
+		})
+		// sendResponse({status:'aaaa'})
+    }
+})
+
